98c9bb9912fd2c1c558e07567f23839b5411e62e
dom31 <dom31@LAPTOP-19N5M1CV>
2022-12-05 21:56:33 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining81/Main.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskDialog = Autodesk.Revit.UI.TaskDialog;
+
+namespace RevitAPITraining81
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            using (var ts = new Transaction(doc, "экспорт в IFC"))
+            {
+                ts.Start();
+                var ifcOption = new IFCExportOptions();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "файл.ifc", ifcOption);
+
+                ts.Commit();
+            }
+            TaskDialog.Show("Cообщение", "Файл IFC сохранен на рабочем столе");
+            return Result.Succeeded;
+        }
+    }
+
+}
